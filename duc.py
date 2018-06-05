2a67c67a6f4fbe5948a46206e95f084146c2f5eb
trongpl94 <namktlc123@gmail.com>
2018-06-06 01:50:32 +0700

    Turtle exercise_A

--- duc.py
+++ duc.py
@@ -1,16 +1,26 @@
-val = input("Enter your Height(cm) and Weight(kg) :").split()
-cm = int(val[0])
-mass = int(val[1])
-meter = cm/100
-bmi = mass/(meter**2)
-if bmi < 16:
-        print("Ur Severely")
-elif 16 < bmi < 18.5:
-        print("Ur Underweight")
-elif 18.5 < bmi < 25:
-        print("Ur Normal")
-elif 25 < bmi < 30:
-        print("Ur OverWeight")
-else:
-        print("Ur Obese")
-
+from turtle import * #Long
+shape("turtle")
+def square():
+        pencolor('orange')#lay mau cam
+        for i in range(0, 4):
+                forward(100)
+                left(90)
+        left(60)#quay goc 60 do de ve tam giac
+def triangle_pentagon():
+        pencolor('blue')#lay mau xanh
+        for j in range(0, 2):
+                forward(100)
+                right(120)
+        right(108)# quay goc 108 do de ve ngu giac
+        for k in range(0, 4):
+                forward(100)
+                left(72)
+        left(120)# quay goc 120 do de ve luc giac
+def hexagon():
+        pencolor('red')#lay mau do
+        for e in range(0, 5):
+                forward(100)
+                right(60)
+square()
+triangle_pentagon()
+hexagon()
