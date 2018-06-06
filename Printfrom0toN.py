7f297cf64b96e2cd24de661bc76f3b063ee6ef9b
trongpl94 <namktlc123@gmail.com>
2018-06-06 14:21:50 +0700

    print 1 0 .... 1

--- Printfrom0toN.py
+++ Printfrom0toN.py
@@ -1,4 +1,3 @@
 n = int(input("Enter a Number"))
 for i in range(0, n):
     print(i, end=" ")
-
---
+++ Total Number 0 and 1.py
@@ -0,0 +1,6 @@
+n = int(input("Enter the total number of 1's and 0's: "))
+for i in range(n):
+    if i % 2 == 1:
+        print(0, end="")
+    if i % 2 == 0:
+        print(1, end="")
